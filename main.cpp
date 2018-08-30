45cd0f5fde70be524843cca3c8796141ab7bb690
Simon Rodriguez <kosua20@gmail.com>
2018-08-30 20:39:42 +0200

    Add tests (srt1 & srt2)

--- main.cpp
+++ main.cpp
@@ -7,11 +7,16 @@
 #include "Shaders.h"
 
 
+void srt1();
+void srt2();
+
 int main()
 {
 	//testFuns();
 	//testBlocks();
-
+	//srt1();
+	//srt2();
+	//return 0;
 	
 	{
 		using namespace fs;
@@ -177,3 +182,188 @@ void testBlocks() {
 	}
 
 }
+
+
+
+
+
+/// Tests
+
+void srt1(){
+	
+	using namespace vs;
+	VertexShader shader0;
+	
+	// Output: UV coordinates
+	/* out INTERFACE {
+	 vec2 uv;
+	 } Out ;*/
+	Out<vec2> Outuv("uv");
+	
+	shader0.main([&]{
+		// vec2 temp = 2.0 * vec2(gl_VertexID == 1, gl_VertexID == 2)
+		vec2 temp = Float(2.0) * vec2(gl_VertexID == Int(1), gl_VertexID == Int(2)) << "";
+		Outuv = temp;
+		// gl_Position[x,y] = 2.0 * temp - 1.0;
+		gl_Position[x,y] = Float(2.0) * temp - vec2(1.0);
+		gl_Position[z,w] = vec2(1.0);
+		
+	});
+	
+	std::cout << shader0.getStr() << std::endl;
+	
+	using namespace fs;
+	FragmentShader shader1;
+	
+	// Input: UV coordinates
+	/* in INTERFACE {
+	 vec2 uv;
+	 } In ;
+	 */
+	In<vec2> Inuv("uv");
+	Out<vec3, Layout<Location<0>>> fragColor("fragColor");
+	// const float M_PI = 3.14159;
+	Float pi(3.14159, "M_PI"); // Is constness possible?
+	// Uniform<sampler2D> screenTexture("screenTexture");
+	// Had to remove the sampler (no myName() implemented and other things)
+	
+	shader1.main([&]{
+		// fragColor = texture(screenTexture, cos(pi*Inuv[y]))[r,b,g];
+		// Had to remove the texture() call.
+		fragColor = vec3(0.0,0.0,cos(pi*Inuv[y]))[r,b,g];
+	});
+	
+	std::cout << shader1.getStr() << std::endl;
+}
+
+void srt2(){
+	using namespace fs;
+	FragmentShader shader1;
+	
+	In<vec2> Inuv("uv");
+	Uniform<mat4> clipToWorld("clipToWorld");
+	Uniform<vec3> viewPos("viewPos");
+	Uniform<vec3> lightDirection("lightDirection");
+	
+	Out<vec3> fragColor("fragColor");
+	Float groundRadius = Float(6371e3) << "groundRadius";
+	Float topRadius = Float(6471e3) << "topRadius";
+	Float sunIntensity = Float(20.0) << "sunIntensity";
+	vec3 sunColor = vec3(1.474, 1.8504, 1.91198) << "sunColor";
+	vec3 kRayleigh = vec3(5.5e-6, 13.0e-6, 22.4e-6) << "kRayleigh";
+	Float kMie = Float(21e-6) << "kMie";
+	Float heightRayleigh = Float(8000.0) <<"heightRayleigh";
+	Float heightMie = Float(1200.0) <<"heightMie";
+	Float gMie = Float(0.758) << "gMie";
+	
+	Float sunAngularRadius = Float(0.04675) << "sunAngularRadius";
+	Float sunAngularRadiusCos = Float(0.998) << "sunAngularRadiusCos";
+	
+	#define SAMPLES_COUNT 16
+	#define M_PI 3.14159265358979323846
+	
+	auto intersects = makeF("intersects", [](vec3 rayOrigin, vec3 rayDir, Float radius, Out<vec2> roots){
+		
+		Float aa = dot(rayDir,rayDir) << "a";
+		Float bb = dot(rayOrigin, rayDir) << "b";
+		Float cc = (dot(rayOrigin, rayOrigin) - radius*radius)  << "c";
+		Float delta = bb*bb - aa*cc << "delta";
+		/* Early return doesn't work?
+		 GL_IF(delta < Float(0.0)){
+		 GL_RETURN False;
+		 }*/
+		Float dsqrt = sqrt(delta) << "";
+		// The unary - doesn't work :(
+		roots = Float(-1.0) * vec2(bb,bb) + dsqrt * vec2(-1.0, 1.0);
+		GL_RETURN Bool(delta > Float(0.0));
+	});
+	
+	
+	auto rayleighPhase = makeF("rayleighPhase", [](Float cosAngle){
+		const Float k = Float(1.0/(4.0*M_PI)) << "k";
+		return k * Float(3.0/4.0) * (Float(1.0) + cosAngle*cosAngle);
+	});
+	
+	auto miePhase = makeF("miePhase", [gMie](Float cosAngle){
+		const Float k = 1.0/(4.0*M_PI);
+		Float g2 = gMie*gMie << "g2";
+		return k * Float(3.0) * (Float(1.0)-g2) / (Float(2.0) * (Float(2.0) + g2)) * (Float(1.0) + cosAngle*cosAngle) / pow(Float(1.0) + g2 - Float(2.0) * gMie * cosAngle, Float(3.0/2.0));
+	});
+	
+	auto computeRadiance = makeF("computeRadiance", [&](vec3 rayOrigin, vec3 rayDir, vec3 sunDir){
+		vec2 interTop("interTop"), interGround("interGround");
+		Bool didHitTop = intersects(rayOrigin, rayDir, topRadius, interTop) << "didHitTop";
+		// Here there is an early return usually.
+		Bool didHitGround = intersects(rayOrigin, rayDir, groundRadius, interGround) << "didHitGround";
+		// Missing min() et ternary.
+		Float distanceToInter("distanceToInter");
+		GL_IF(didHitGround){
+			distanceToInter = interGround[x];
+		} GL_ELSE {
+			distanceToInter = 0.0;
+		}
+		Float stepSize = (distanceToInter - interTop[x])/Float(SAMPLES_COUNT) << "stepSize";
+		Float cosViewSun = dot(rayDir, sunDir) << "cosViewSun";
+		Float rayleighDist = Float(0.0) << "rayleighDist";
+		Float mieDist = Float(0.0) << "mieDist";
+		vec3 rayleighScatt = vec3(0.0) << "rayleighScatt";
+		vec3 mieScatt = vec3(0.0) << "mieScatt";
+		vec3 transmittance = vec3(0.0) << "transmittance";
+		
+		GL_FOR(Int i(0,"i"); i < SAMPLES_COUNT; ++i){
+			vec3 currPos = rayOrigin + (Float(i)+Float(0.5)) * stepSize * rayDir << "currPos";
+			Float currHeight = length(currPos) - groundRadius << "currHeight";
+			GL_IF(i == Int(SAMPLES_COUNT-1) && currHeight < Float(0.0)){
+				currHeight = 0.0;
+			}
+			// Had to wrap - -> -1.0 -> Float(-1.0)
+			Float rayleighStep = exp(Float(-1.0)*currHeight/heightRayleigh) * stepSize << "rayleighStep";
+			Float mieStep = exp(Float(-1.0)*currHeight/heightMie) * stepSize << "mieStep";
+			rayleighDist += rayleighStep;
+			mieDist += mieStep;
+			// Couldn't mix Float and vec3: kMie*mieDist + kRayleigh*rayleighDist
+			vec3 directAttenuation = exp(Float(-1.0)*(kRayleigh * rayleighDist)) << "directAttenuation";
+			// Missing parenthesis in the generated code.
+			Float relativeHeight = (length(currPos) - groundRadius) / (topRadius - groundRadius) << "relativeHeight";
+			Float relativeCosAngle = Float(-0.5)*sunDir[y]+Float(0.5) << "relativeCosAngle";
+			// same thing, can't mix float and vec2 when creating in-expression vec2.
+			vec2 attenuationUVs = vec2(relativeHeight, relativeCosAngle) << "attenuationUVs";
+			// attenuationUVs *= Float(1.0-1.0/512.0);
+			attenuationUVs += Float(0.5/512.0);
+			// vec3 secondaryAttenuation = texture(screenTexture, attenuationUVs).rgb;
+			vec3 secondaryAttenuation = attenuationUVs[x,y,x];
+			// vec3 attenuation = directAttenuation * secondaryAttenuation << "attenuation";
+			vec3 attenuation = directAttenuation << "attenuation";
+			rayleighScatt += rayleighStep * attenuation;
+			mieScatt += mieStep * attenuation;
+			transmittance += directAttenuation;
+		}
+		
+		// Should be: vec3 rayleighParticipation = kRayleigh * rayleighPhase(cosViewSun) * rayleighScatt;
+		// but error on vec3*vec3
+		vec3 rayleighParticipation = rayleighPhase(cosViewSun) *  rayleighScatt << "rayleighParticipation";
+		vec3 mieParticipation = kMie * miePhase(cosViewSun) * mieScatt << "mieParticipation";
+		
+		vec3 sunRadiance = vec3(0.0) << "sunRadiance";
+		Bool didHitGroundForward = (didHitGround && interGround[y] > Float(0)) << "didHitGroundForward";
+		GL_IF(!didHitGroundForward && dot(rayDir, sunDir) > sunAngularRadiusCos){
+			sunRadiance = sunColor / (Float(M_PI) * sunAngularRadius * sunAngularRadius);
+		}
+		
+		return sunIntensity * (rayleighParticipation + mieParticipation) + sunRadiance + transmittance;
+	});
+	
+	
+	shader1.main([&]{
+		// This one causes an error on createDummy.
+		//vec4 clipVertex = vec4(Float(-1.0)+Float(2.0)*Inuv, 0.0, 1.0);
+		vec4 clipVertex = vec4(vec2(-1.0)+Float(2.0)*Inuv, 0.0, 1.0) << "clipVertex";
+		vec3 viewRay = normalize((clipToWorld * clipVertex)[x,y,z]) << "viewRay";
+		vec3 planetSpaceViewPos = viewPos + vec3(0.0, 6371e3, 0.0) + vec3(0.0,1.0,0.0) << "planetSpaceViewPos";
+		vec3 atmosphereColor = computeRadiance(planetSpaceViewPos, viewRay, lightDirection) << "atmosphereColor";
+		fragColor = atmosphereColor;
+	});
+	
+	std::cout << shader1.getStr() << std::endl;
+}
+
