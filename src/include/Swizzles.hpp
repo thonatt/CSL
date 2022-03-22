79b3ba9a985196b86fd96b6b6b59cfc216c15428
tthonat <thonat.t@gmail.com>
2022-03-22 13:01:00 +0100

    Swizzles cleaning

--- src/include/Swizzles.hpp
+++ src/include/Swizzles.hpp
@@ -4,10 +4,10 @@
 #include <cstddef>
 #include <type_traits>
 
-namespace csl {
-
-	namespace detail_swizzling {
-
+namespace csl
+{
+	namespace swizzling
+	{
 		template<char...>
 		constexpr bool is_different_from = false;
 
@@ -28,25 +28,25 @@ namespace csl {
 		using SwizzleSTPQ = CharSeq<'s', 't', 'p', 'q'>;
 
 		template<char c>
-		constexpr std::size_t SwizzleId = 0;
+		constexpr std::size_t SwizzleIndex = 0;
 
-		template<> constexpr std::size_t SwizzleId<'r'> = 1;
-		template<> constexpr std::size_t SwizzleId<'g'> = 2;
-		template<> constexpr std::size_t SwizzleId<'b'> = 3;
-		template<> constexpr std::size_t SwizzleId<'a'> = 4;
+		template<> constexpr std::size_t SwizzleIndex<'r'> = 1;
+		template<> constexpr std::size_t SwizzleIndex<'g'> = 2;
+		template<> constexpr std::size_t SwizzleIndex<'b'> = 3;
+		template<> constexpr std::size_t SwizzleIndex<'a'> = 4;
 
-		template<> constexpr std::size_t SwizzleId<'x'> = 1;
-		template<> constexpr std::size_t SwizzleId<'y'> = 2;
-		template<> constexpr std::size_t SwizzleId<'z'> = 3;
-		template<> constexpr std::size_t SwizzleId<'w'> = 4;
+		template<> constexpr std::size_t SwizzleIndex<'x'> = 1;
+		template<> constexpr std::size_t SwizzleIndex<'y'> = 2;
+		template<> constexpr std::size_t SwizzleIndex<'z'> = 3;
+		template<> constexpr std::size_t SwizzleIndex<'w'> = 4;
 
-		template<> constexpr std::size_t SwizzleId<'s'> = 1;
-		template<> constexpr std::size_t SwizzleId<'t'> = 2;
-		template<> constexpr std::size_t SwizzleId<'p'> = 3;
-		template<> constexpr std::size_t SwizzleId<'q'> = 4;
+		template<> constexpr std::size_t SwizzleIndex<'s'> = 1;
+		template<> constexpr std::size_t SwizzleIndex<'t'> = 2;
+		template<> constexpr std::size_t SwizzleIndex<'p'> = 3;
+		template<> constexpr std::size_t SwizzleIndex<'q'> = 4;
 
 		template<char ... chars>
-		constexpr std::size_t SwizzleHighestComponent = std::max({ SwizzleId<chars> ... });
+		constexpr std::size_t HighestComponent = std::max({ SwizzleIndex<chars> ... });
 
 		template<char c, typename T>
 		constexpr bool is_in_set = false;
@@ -64,63 +64,63 @@ namespace csl {
 
 		template<char c>
 		using SwizzleSet = typename SwizzleGetter<c, SwizzleRGBA, SwizzleXYZW, SwizzleSTPQ>::Type;
-
 	}
 
 	template<char c, char... chars>
-	class Swizzle {
-
+	class Swizzle
+	{
 	public:
-		static constexpr bool Unique = detail_swizzling::are_unique<c, chars...>;
+		static constexpr bool Unique = swizzling::are_unique<c, chars...>;
 		static constexpr std::size_t Size = 1 + sizeof...(chars);
-		static constexpr std::size_t HighestComponent = detail_swizzling::SwizzleHighestComponent<c, chars...>;
+		static constexpr std::size_t HighestComponent = swizzling::HighestComponent<c, chars...>;
 
-		constexpr Swizzle() { }
+		constexpr Swizzle() = default;
 
 		template<char other_c>
-		constexpr Swizzle<c, chars..., other_c>
-			operator,(Swizzle<other_c>) const
+		constexpr Swizzle<c, chars..., other_c> operator,(Swizzle<other_c>) const
 		{
-			return { };
+			return {};
 		}
 
 	private:
-		using Set = detail_swizzling::SwizzleSet<c>;
+		using Set = swizzling::SwizzleSet<c>;
 
 		static_assert(Size <= 4, "Maximum swizzling size is 4");
 		static_assert(Set(), "Invalid swizzle set");
-		static_assert((std::is_same_v<Set, detail_swizzling::SwizzleSet<chars>>&& ... && true), "Swizzle sets cannot be mixed");
-
+		static_assert(((std::is_same_v<Set, swizzling::SwizzleSet<chars>>)&& ...), "Swizzle sets cannot be mixed");
 	};
 
-	namespace swizzles {
-
-		namespace rgba {
+	namespace swizzles
+	{
+		namespace rgba
+		{
 			constexpr Swizzle<'r'> r;
 			constexpr Swizzle<'g'> g;
 			constexpr Swizzle<'b'> b;
 			constexpr Swizzle<'a'> a;
 		}
 
-		namespace xyzw {
+		namespace xyzw
+		{
 			constexpr Swizzle<'x'> x;
 			constexpr Swizzle<'y'> y;
 			constexpr Swizzle<'z'> z;
 			constexpr Swizzle<'w'> w;
 		}
 
-		namespace stpq {
+		namespace stpq
+		{
 			constexpr Swizzle<'s'> s;
 			constexpr Swizzle<'t'> t;
 			constexpr Swizzle<'p'> p;
 			constexpr Swizzle<'q'> q;
 		}
 
-		namespace all {
+		namespace all
+		{
 			using namespace rgba;
 			using namespace xyzw;
 			using namespace stpq;
 		}
-
 	}
 }
